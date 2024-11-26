52c311ac9d91a279145ccd1a5f87c7bda58491c5
blountdj <blountdj@gmail.com>
2024-11-26 14:18:30 +0100

    updates

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
   barbaDebug: false,
-  path: 'https://cdn.jsdelivr.net/gh/blountdj/portfolio-site@v2/',
+  path: 'https://cdn.jsdelivr.net/gh/blountdj/portfolio-site@v3/',
   // jsPath: 'js/',
   jsPath: 'min/js/',
   // min: '',
